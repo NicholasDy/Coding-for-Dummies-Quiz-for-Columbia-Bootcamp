102f7384618a467fd1fcf4238348c6cb5ae002ec
Nick <nicholasdy272@gmail.com>
2021-02-22 20:18:49 -0500

    pushing the index finished css files to the repo

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// declare all my variables 
+var startBtn = document.querySelector(".start-btn")
+var noBTn = document.querySelector(".no-btn")
+var resetBtn = document.querySelector(".reset-btn")
+var questionEl = document.querySelector(".question")
+var questionBox = document.querySelector(".questionbox")
+var timerEl = document.querySelector(".timer")
+var questionsLeftEL = document.querySelector(".questionsleft")
+var correctEl = document.querySelector(".correct")
+var incorrectEl = document.querySelector(".incorrect")
+
+// question bank
+
+// function init 
+
+// timer  function 
+
+// not prepared function
+
+// yes question
+
+// reset btn
+
+// you passed function
+
+// you failed function
+
+// questions function 
+
+// getting wins 
+
+// getting loses
+
+
+
+// listener function to start the quiz
+
